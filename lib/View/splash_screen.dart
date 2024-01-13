23898ce3e54d0776c61b47e2f99284233e32f5a2
dhyey <dhyeyrabadiya2030@gmaol.com>
2024-01-13 12:02:33 +0530

    Initial commit

---
+++ lib/View/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import '../Model/const.dart';
+import 'home_page.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, homePage);
+
+
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            color: Colors.white
+        ),
+        child: Center(
+          child: Image.asset("images/seo.png",height: 100,width: 100,),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+import 'package:mirrorwall/View/home_page.dart';
+import 'package:provider/provider.dart';
+
+import 'Controller/bookmark_provider.dart';
+import 'Controller/connectivity_provider.dart';
+import 'Model/const.dart';
+import 'View/splash_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ConnectivityProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => BookMarkProvider(),
+        ),
+      ],
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Hello",
+            // home: HomeScreen(),
+            routes: {
+              "/": (context) => SplashScreen(),
+              homePage:(context) => Home_page(url: "https://www.google.com/"),
+            },
+          );
+        }
+      ),
+    );
+  }
+}
+
